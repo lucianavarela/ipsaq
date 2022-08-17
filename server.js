5056d97d8279b272708f56f6ce26099e259dc0ca
luli <lucianafvarela@gmail.com>
2022-08-16 21:40:41 -0300

    cambios

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/ipsaq'));
+app.use(express.static(__dirname + '/dist/ipsaq-website'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ipsaq/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ipsaq-website/index.html'));
 });
 
 // default Heroku port
