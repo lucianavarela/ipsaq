de4dde0488c6c3ba9712c0581f48d83724be55b6
Luli <lucianafvarela@gmail.com>
2023-03-06 00:53:31 -0300

    fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  supabaseUrl: 'https://irptbmgsjzrpeolgfbqm.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlycHRibWdzanpycGVvbGdmYnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE5ODAyMDgsImV4cCI6MTk3NzU1NjIwOH0.Cq6-kadMr5P7Lm6d1XbzhNYIMxPfz7x_Lzlt96QpZI0'
 };
